330a1582a27bb8b647fe46d124bc1aaed1c752e5
Unitycorn <flaig.m@gmx.de>
2024-11-09 12:21:56 +0100

    Fixed Error in daily API request

--- components/AchievementFilter.jsx
+++ components/AchievementFilter.jsx
@@ -5,40 +5,42 @@ export default function AchievementFilter({
   setFractalTier,
 }) {
   const contentTypes = Object.keys(dailies);
-  return (
-    <section className="daily_filters">
-      <label htmlFor="contentType">Wähle eine Kategorie:</label>
-      <select
-        id="contentType"
-        name="contentType"
-        onChange={(e) => setContent(e.currentTarget.value)}
-      >
-        {contentTypes.map((e) => (
-          <option key={e} value={e}>
-            {e}
-          </option>
-        ))}
-      </select>
-      {content == 'fractals' ? (
-        <>
-          <label htmlFor="fractalTier">Wähle eine Schwierigkeitsstufe:</label>
-          <select
-            id="farctalTier"
-            name="fractalTier"
-            onChange={(e) => setFractalTier(e.currentTarget.value)}
-          >
-            <option value="">Alle</option>
-            <option value="Rang 1">Tier 1</option>
-            <option value="Rang 2">Tier 2</option>
-            <option value="Rang 3">Tier 3</option>
-            <option value="Rang 4">Tier 4</option>
-          </select>
-        </>
-      ) : (
-        /* Wichtig: Den Schwierigkeitsfilter (wieder) zurücksetzen wenn vom Content Fraktale wieder 
+  if (dailies[content]) {
+    return (
+      <section className="daily_filters">
+        <label htmlFor="contentType">Wähle eine Kategorie:</label>
+        <select
+          id="contentType"
+          name="contentType"
+          onChange={(e) => setContent(e.currentTarget.value)}
+        >
+          {contentTypes.map((e) => (
+            <option key={e} value={e}>
+              {e}
+            </option>
+          ))}
+        </select>
+        {content == 'fractals' ? (
+          <>
+            <label htmlFor="fractalTier">Wähle eine Schwierigkeitsstufe:</label>
+            <select
+              id="farctalTier"
+              name="fractalTier"
+              onChange={(e) => setFractalTier(e.currentTarget.value)}
+            >
+              <option value="">Alle</option>
+              <option value="Rang 1">Tier 1</option>
+              <option value="Rang 2">Tier 2</option>
+              <option value="Rang 3">Tier 3</option>
+              <option value="Rang 4">Tier 4</option>
+            </select>
+          </>
+        ) : (
+          /* Wichtig: Den Schwierigkeitsfilter (wieder) zurücksetzen wenn vom Content Fraktale wieder 
         in einen anderen Content gewechselt wird, da ansonsten nichts mehr ausgegeben wird, weil nach "Rang x" gesucht wird*/
-        setFractalTier('')
-      )}
-    </section>
-  );
+          setFractalTier('')
+        )}
+      </section>
+    );
+  }
 }
--- components/AchievementList.jsx
+++ components/AchievementList.jsx
@@ -1,7 +1,8 @@
+import Image from 'next/image';
 export default function AchievementList({ dailies, content, fractalTier }) {
   const cssClass = `dailies daily_${content}`;
 
-  if (dailies[content].length) {
+  if (dailies[content]) {
     return dailies[content].map((e) => {
       if (
         /*Early return wenn der Titel nicht "Rang X" beinhaltet*/
@@ -12,7 +13,7 @@ export default function AchievementList({ dailies, content, fractalTier }) {
       }
       return (
         <section key={e.id} className={cssClass}>
-          <img
+          <Image
             className="daily-icon"
             src="/img/Daily_Achievement.png"
             alt="daily-icon"
@@ -32,6 +33,23 @@ export default function AchievementList({ dailies, content, fractalTier }) {
       );
     });
   } else {
-    return <h3>Keine Dailies gefunden!</h3>;
+    return (
+      <div>
+        <h3>Keine Dailies gefunden!</h3>
+        <h2>Update 11/2024:</h2>
+        <p>
+          Aufgrund eines Systemwechsels wurden die Daily-Achievements 2023 aus
+          dem Spiel entfernt und gegen ein neues System ersetzt.
+          <br />
+          Der Code für das alte System wurde aufgrund der Nachvollziehbarkeit
+          vollständing behalten.
+          <br />
+          <br />
+          <br />
+          ToDo: Update der REST-API-Abfragen auf die neue Version / das neue
+          System!
+        </p>
+      </div>
+    );
   }
 }
